ec23dae5681d19c438297b729d9802d3fdda5638
DannyJ07 <djyanchapanta@espe.edu.ec>
2024-05-28 11:29:37 -0500

    Validacion de salto

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float velocidad = 2;
+    public GameObject col;
+    //atributo de tipo publico 
+    public Renderer fondo;
+
+    public List<GameObject> cols;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Crear Mapa 
+        for(int i=0; i < 21; i++)
+        {
+           cols.Add(Instantiate(col, new Vector2(-10 + i, -3), Quaternion.identity));
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.015f, 0) * Time.deltaTime;
+
+        // Mover mapa 
+        for (int i = 0; i < cols.Count; i++)
+        {
+            if (cols[i].transform.position.x <= -10)
+            {
+                cols[i].transform.position = new Vector3(10, -3, 0);
+            }
+            //cols.Add(Instantiate(col, new Vector2(-10 + 1, i - 3), Quaternion.identity));
+            cols[i].transform.position = cols[i].transform.position + new Vector3(-1, 0,0)* Time.deltaTime*velocidad;
+        }
+    }
+}
---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    public float fuerzaSalto;
+    private Rigidbody2D rigidbody2D;
+    private Animator animator;
+    private bool estaSaltando = false; // Variable para controlar el estado del salto
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !estaSaltando) // Verifica si se presiona "Espacio" y no está saltando
+        {
+            animator.SetBool("estaSaltando", true);
+            rigidbody2D.AddForce(new Vector2(0, fuerzaSalto));
+            estaSaltando = true; // Marca como que está saltando
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Suelo")
+        {
+            animator.SetBool("estaSaltando", false);
+            estaSaltando = false; // Marca como que está en el suelo al tocar el suelo
+        }
+    }
+}
